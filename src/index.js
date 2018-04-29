49c377d1e4a0e1a445a633ae4e43ed00c2038a1f
Brenda Johnson <bren.john55@gmail.com>
2018-04-28 23:58:28 -0400

    number key to display

--- src/index.js
+++ src/index.js
@@ -1,1 +1,45 @@
 console.log('hello world');
+const calculator = document.querySelector('.calculator');
+const keys = calculator.querySelector('.calculator__keys');
+const display = document.querySelector('.calculator__display');
+
+keys.addEventListener('click', e => {
+  if (e.target.matches('button')) {
+    const key = e.target;
+    const action = key.dataset.action;
+    const keyContent = key.textContent;
+    const displayedNum = display.textContent;
+  }
+
+  const key = e.target;
+  const action = key.dataset.action;
+
+  if (!action) {
+    if (displayedNum === '0') {
+      display.textContent = keyContent;
+    } else {
+      display.textContent = displayedNum + keyContent;
+      //displayNum is not defined. Scope or bracket?
+    }
+  }
+
+  if (
+    action === 'add' ||
+    action === 'subtract' ||
+    action === 'multiply' ||
+    action === 'divide'
+  ) {
+    console.log('operator key!');
+  }
+  if (action === 'decimal') {
+    console.log('decimal key!');
+  }
+
+  if (action === 'clear') {
+    console.log('clear key!');
+  }
+
+  if (action === 'calculate') {
+    console.log('equal key!');
+  }
+});
